ed3d26c3ceab3f0748c4709bf4553321bcd9f461
Daquan Mac <Daquan@Daquans-MacBook-Air.local>
2017-08-03 12:29:29 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+// Since the `data.js` file is loaded in your `index.html` before this one,
+// you have access to the `data` variable that contains the Etsy Store data.
+// Open your `index.html` page in your browser and you can verify that the following
+// is properly working. The `data` variable is an array with 25 items in it
+console.log(data);
+
+
+
+// 1: Show me how to calculate the average price of all items.
+function question1 () {
+  // Answer:
+  let average = 0;
+   let total = 0;
+   for (var i = 0; i < data.length; i++) {
+     total = total + data[i].price;
+   }
+   average = total / data.length;
+   console.log("The average price is $" + average.toFixed(2));
+ }
+// 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
+function question2 () {
+  // Answer:
+  for (let i = 0; i < data.length; i++) {
+     if (data[i].price >= 14.00 && data[i].price <= 18.00) {
+       console.log(data[i].title);
+     }
+   }
+ }
+
+
+
+// 3: Which item has a "GBP" currency code? Display it's name and price.
+function question3 () {
+  // Answer:
+  for (let i=0; i < data.length; i++){
+    if (data[i].currency_code === "GBP"){
+      console.log(data[i].title + " "+ "cost" + " " + data[i].price + " " + "pounds.");
+    }
+  }
+}
+
+
+// 4: Display a list of all items who are made of wood.
+function question4 () {
+  // Answer:
+    for (var i = 0; i < data.length; i++) {
+     for (var n = 0; n < data[i].materials.length; n++) {
+       if (data[i].materials[n] === "wood") {
+         console.log(data[i].title + " is made of wood.");
+       }
+     }
+   }
+
+}
+
+
+// 5: Which items are made of eight or more materials?
+//    Display the name, number of items and the items it is made of.
+function question5 () {
+  // Answer:
+  let numMaterials;
+
+data.forEach(function(e){
+  if(e.materials.length >= 8){
+    console.log(e.title + " has " + e.materials.length + " materials: ");
+    e.materials.forEach(function(f){
+      console.log("- "+ f);
+    })
+  }
+})
+
+}
+
+
+// 6: How many items were made by their sellers?
+// Answer:
+function question6 () {
+  // Answer:
+  let total = 0;
+  data.forEach(function(e){
+    if(e.who_made == "i_did"){
+      total++;
+    }
+  })
+  console.log(total +" items were made by their sellers.");
+}
